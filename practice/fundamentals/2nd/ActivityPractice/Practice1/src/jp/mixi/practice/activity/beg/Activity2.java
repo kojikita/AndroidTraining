02b08f60cb2f79a5775a0d5c206c0242af86ac25
koji.kita <koji.kita@mixi.co.jp>
2013-04-23 15:24:23 +0900

    [practice03] Activity and Fragment

---
+++ practice/fundamentals/2nd/ActivityPractice/Practice1/src/jp/mixi/practice/activity/beg/Activity2.java
@@ -0,0 +1,12 @@
+package jp.mixi.practice.activity.beg;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class Activity2 extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+    }
+}
