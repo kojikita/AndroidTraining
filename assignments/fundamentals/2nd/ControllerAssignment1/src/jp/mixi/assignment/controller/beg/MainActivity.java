d1131ec91eb2f27ac8c7f91a7f087783eed4c23e
koji.kita <koji.kita@mixi.co.jp>
2013-05-07 20:42:08 +0900

    [assigntment03] No.5

--- assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -1,9 +1,10 @@
 
 package jp.mixi.assignment.controller.beg;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 /**
  * TODO: 課題1
@@ -19,6 +20,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Toast.makeText(this, "Toast example", Toast.LENGTH_LONG).show();
     }
 
     @Override
