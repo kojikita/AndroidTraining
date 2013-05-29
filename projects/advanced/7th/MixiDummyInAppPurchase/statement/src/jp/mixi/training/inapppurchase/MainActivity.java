30af684f76383382bd7cb0a718d25686f60923e3
koji.kita <koji.kita@mixi.co.jp>
2013-05-29 20:20:44 +0900

    購入できたところまで

--- projects/advanced/7th/MixiDummyInAppPurchase/statement/src/jp/mixi/training/inapppurchase/MainActivity.java
+++ projects/advanced/7th/MixiDummyInAppPurchase/statement/src/jp/mixi/training/inapppurchase/MainActivity.java
@@ -1,19 +1,52 @@
 package jp.mixi.training.inapppurchase;
 
+import java.util.ArrayList;
+
+import com.android.vending.billing.IInAppBillingService;
+
 import jp.mixi.training.inapppurchase.helper.DummySku;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.os.IBinder;
 
 public class MainActivity extends FragmentActivity {
 	private static final String TAG = MainActivity.class.getSimpleName();
 
+    IInAppBillingService mService;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ServiceConnection mServiceConn = new ServiceConnection() {
+		   @Override
+		   public void onServiceDisconnected(ComponentName name) {
+		       mService = null;
+		   }
+
+		   @Override
+		   public void onServiceConnected(ComponentName name, IBinder service) {
+		       mService = IInAppBillingService.Stub.asInterface(service);
+		   }
+		};
+		
+	    bindService(new
+	        Intent("com.android.vending.billing.InAppBillingService.BIND"),
+	            mServiceConn, Context.BIND_AUTO_CREATE);
+	    
+	    
+
 	}
 
 	private void disableActionViews () {
@@ -36,11 +69,29 @@ public class MainActivity extends FragmentActivity {
 
 	@Override
 	public void onDestroy() {
-		super.onDestroy();
+	    super.onDestroy();
 	}
 
 	private void startPurchase (final DummySku skuToPurchase) {
 		//IMP here
+	    Bundle buyIntentBundle = null;
+	    try {
+            buyIntentBundle = mService.getBuyIntent(3, getPackageName(),
+                    skuToPurchase.getId(), "inapp", "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+	    try {
+            startIntentSenderForResult(pendingIntent.getIntentSender(),
+                    1001, new Intent(), Integer.valueOf(0), Integer.valueOf(0),
+                    Integer.valueOf(0));
+        } catch (SendIntentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    
 		Log.w(TAG, "purchase not implemented");
 	}
 
